e5effa1ef39ba6915db6db183d91b365bf043f6e
Ileana Marquez <168607179+ileamarq@users.noreply.github.com>
2024-06-25 02:16:42 +0000

    primer commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
-
+import requests
+import os
 
 st.title("Asistente generador de recetas personalizadas")
-
 # Campo de texto para ingresar ingredientes
 ingredientes = st.text_area("Ingresa la lista de ingredientes (separados por comas)")
 
@@ -17,3 +17,28 @@ categoria_elegida = categoria_seleccionada
 # Mostrar los valores seleccionados
 st.write(f"Ingredientes ingresados: {ingredientes_lista}")
 st.write(f"Categoría seleccionada: {categoria_elegida}")
+
+# Datos para la solicitud
+base_url = "https://api.dify.ai/v1"
+path = "/completion-messages"
+my_secret = os.environ['TU_API_KEY']
+
+
+data = {
+    "text": text,
+    "categoria": categoria
+}
+
+
+headers = {
+    "Authorization": f"Bearer {my_secret}",
+    "Content-Type": "application/json"
+}
+
+# Realizar la solicitud POST
+url_completa = base_url + path
+response = requests.post(url_completa, json=data, headers=headers)
+
+# Imprimir la respuesta
+print(response.text)
+
