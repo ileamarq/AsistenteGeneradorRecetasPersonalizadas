f52b66b954d38e009ec541578bee2eda5a0ba7e6
Ileana Marquez <168607179+ileamarq@users.noreply.github.com>
2024-06-27 13:39:56 +0000

    segundo commt

--- main.py
+++ main.py
@@ -4,41 +4,44 @@ import os
 
 st.title("Asistente generador de recetas personalizadas")
 # Campo de texto para ingresar ingredientes
-ingredientes = st.text_area("Ingresa la lista de ingredientes (separados por comas)")
+text = st.text_area("Ingresa la lista de ingredientes (separados por comas)")
 
 # Input de selección múltiple para la categoría de receta
-categorias = ["Aperitivo", "Ensalada", "Plato principal"]
-categoria_seleccionada = st.selectbox("Selecciona la categoría de la receta:", categorias)
-
-# Guardar los valores en variables respectivas
-ingredientes_lista = ingredientes.split(",") if ingredientes else []
-categoria_elegida = categoria_seleccionada
-
-# Mostrar los valores seleccionados
-st.write(f"Ingredientes ingresados: {ingredientes_lista}")
-st.write(f"Categoría seleccionada: {categoria_elegida}")
+categoria = ["Aperitivo", "Ensalada", "Plato principal"]
+categoria_seleccionada = st.selectbox("Selecciona la categoría de las recetas:", categoria)
 
 # Datos para la solicitud
 base_url = "https://api.dify.ai/v1"
 path = "/completion-messages"
-my_secret = os.environ['TU_API_KEY']
-
-
-data = {
-    "text": text,
-    "categoria": categoria
-}
-
+my_secret = os.environ['DIFY_APP_KEY']
 
 headers = {
     "Authorization": f"Bearer {my_secret}",
     "Content-Type": "application/json"
 }
 
+data = {
+    "inputs": {
+     "text": text,
+     "categoria": categoria_seleccionada 
+   },
+
+"response_mode": "blocking",
+"user": "Buscadorrecetas"
+}
+
 # Realizar la solicitud POST
 url_completa = base_url + path
-response = requests.post(url_completa, json=data, headers=headers)
-
-# Imprimir la respuesta
-print(response.text)
-
+if st.button('Consultar'):
+  response = requests.post(url_completa, json=data, headers=headers)
+  
+  if response.status_code == 200:
+   st.success("Recetas generada exitosamente")
+   
+   result = response.json()
+  # Imprimir la respuesta
+   st.markdown("### Aqui tienes tus tres recetas:")
+   st.markdown(result["answer"])
+  else:
+   st.error("Error al generar la receta")
+  
